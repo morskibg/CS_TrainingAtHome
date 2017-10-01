ebcfa3bd85c23810e7e547bbf0c0d987ca1d2fcd
morskibg <dimityrp@yahoo.com>
2017-10-01 21:06:31 +0200

    final

---
+++ Dict_Lab/2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine().Split(' ').Select(double.Parse).OrderBy(x => x).ToList();
+            Dictionary<double, int> wordsCount = new Dictionary<double, int>();
+            foreach (var num in nums)
+            {
+                if (wordsCount.ContainsKey(num))
+                {
+                    wordsCount[num]++;
+                }
+                else
+                {
+                    wordsCount[num] = 1;
+                }
+            }
+            foreach (var kvp in wordsCount)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+        }
+    }
+}
