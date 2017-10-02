240c157745c62f6c652ae3aa3cdde77274ca9836
morskibg <dimityrp@yahoo.com>
2017-10-02 17:48:34 +0300

    +

---
+++ Dict_Exercise/3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, long> database = new Dictionary<string, long>();
+            while(input != "Over")
+            {
+                long number = 0;
+                char[] delimiters = " :".ToCharArray();
+                string[] nameAndNumber = input.Split(delimiters).ToArray();
+                string name = "";
+                if (long.TryParse(nameAndNumber.First(),out number))
+                {
+                    number = long.Parse(nameAndNumber.First());
+                    database[nameAndNumber.Last()] = number;
+                }
+                else
+                {
+                    number = long.Parse(nameAndNumber.Last());
+                    database[nameAndNumber.First()] = number;
+                }
+                input = Console.ReadLine();
+            }
+            foreach(var item in database.OrderBy(kvp => kvp.Key))
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+    }
+}
