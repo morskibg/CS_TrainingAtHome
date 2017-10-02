3d8b30e212809ea1255262bd5c4840ec800633fe
morskibg <dimityrp@yahoo.com>
2017-10-02 20:40:52 +0200

    +

---
+++ Dict_Exercise/4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, long> inventory = new Dictionary<string, long>();
+            while (input != "exam time")
+            {
+                string[] words = input.Split(' ').ToArray();
+                string command = words.First();
+                if (command == "shopping")
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                string item = words[1];
+                long qty = long.Parse(words[2]);
+                if (command == "stock")
+                {
+                    if (inventory.ContainsKey(item))
+                    {
+                        inventory[item] += qty;
+                    }
+                    else
+                    {
+                        inventory[item] = qty;
+                    }
+                }
+                else if (command == "buy")
+                {
+                    if (inventory.ContainsKey(item))
+                    {
+                        if (inventory[item] == 0)
+                        {
+                            Console.WriteLine($"{item} out of stock");
+                        }
+                        else if (inventory[item] > qty)
+                        {
+                            inventory[item] -= qty;
+                        }
+                        else
+                        {
+                            inventory[item] = 0;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{item} doesn't exist");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in inventory.Where(kvp => kvp.Value != 0))
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ Dict_Exercise/5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void parseStringToUserAndPasswordDictionary(string input, ref Dictionary<string, string> usersDatabase)
+        {
+            char[] delimiters = " ->".ToCharArray();
+            string[] userAndPassword = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            string name = userAndPassword.First();
+            string password = userAndPassword.Last();
+            usersDatabase[name] = password;
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, string> userDatabase = new Dictionary<string, string>();
+            int unsuccessifulLogins = 0;
+            while (input != "login" && input != "end")
+            {
+                parseStringToUserAndPasswordDictionary(input, ref userDatabase);
+                input = Console.ReadLine();
+            }
+            input = Console.ReadLine();
+            while (input != "end")
+            {
+                Dictionary<string, string> currentLoginAttempt = new Dictionary<string, string>();
+                parseStringToUserAndPasswordDictionary(input, ref currentLoginAttempt);
+                string currUser = currentLoginAttempt.Keys.First() ;
+                string currPassword = currentLoginAttempt.Values.First();
+                if (userDatabase.ContainsKey(currUser))
+                {
+                    if (userDatabase[currUser] == currPassword)
+                    {
+                        Console.WriteLine($"{currUser}: logged in successfully");
+                    }
+                    else
+                    {
+                        ++unsuccessifulLogins;
+                        Console.WriteLine($"{currUser}: login failed");
+                    }
+                }
+                else
+                {
+                    ++unsuccessifulLogins;
+                    Console.WriteLine($"{currUser}: login failed");
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"unsuccessful login attempts: {unsuccessifulLogins}");
+        }
+    }
+}
