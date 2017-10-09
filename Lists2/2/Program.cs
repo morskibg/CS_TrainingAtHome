25098617c77187c8972500a7975ad9fd1fb678fd
morskibg <dimityrp@yahoo.com>
2017-10-09 22:57:44 +0200

    +

---
+++ Lists2/2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delim = "|".ToCharArray();
+            string[] parts = Console.ReadLine().Split(delim, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<int> nums = new List<int>();
+            for (int i = parts.Length - 1; i >= 0; --i)
+            {
+                List<int> tempList = parts[i]
+                    .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+                
+                foreach (int num in tempList)
+                {
+                    nums.Add(num);
+                }
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
---
+++ Lists2/Lists2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .Where(x => x > 0)
+                .Reverse()
+                .ToList();
+            if (nums.Count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
