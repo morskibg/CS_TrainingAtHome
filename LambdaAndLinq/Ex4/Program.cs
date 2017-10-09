a960f50c4821c28265d7111baa627125d1712637
morskibg <dimityrp@yahoo.com>
2017-10-09 19:14:15 +0200

    +

--- LambdaAndLinq/Ex4/Program.cs
+++ LambdaAndLinq/Ex4/Program.cs
@@ -20,18 +20,26 @@ namespace Ex4
                     playersDatabase[rawData[1]] =
                         new Dictionary<string, int> { { rawData[0], int.Parse(rawData[2]) } };
                 }
-                else if(playersDatabase[rawData[1]].Count <= 3){
+                else if (playersDatabase[rawData[1]].Count < 3)
+                {
                     playersDatabase[rawData[1]].Add(rawData[0], int.Parse(rawData[2]));
                 }
                 line = Console.ReadLine();
             }
-            foreach( var currTeam in playersDatabase
-                .Where(kvp => kvp.Value.Count >= 3)
-                .OrderByDescending(kvp => kvp.Value.Values.Sum())
-                .ThenByDescending(kvp => kvp.Value.Values))
+            int counter = 0;
+            foreach (var team in playersDatabase
+                .Where(kvp => kvp.Value.Count == 3)
+                .OrderByDescending(kvp => kvp.Value.Values.Sum()))
             {
-                Console.WriteLine($"{currTeam.Key} - {currTeam.Value.Values.Sum()}");
+                ++counter;
+                Console.WriteLine($"{counter}. {team.Key}; Players:");
+                foreach (var player in team.Value
+                    .OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"###{player.Key}: {player.Value}");
+                }
             }
+
         }
     }
 }
