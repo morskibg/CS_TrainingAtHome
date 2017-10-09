ea2f5f9a2968b909b1780b3633da45ea3adbfb03
morskibg <dimityrp@yahoo.com>
2017-10-09 17:44:24 +0300

    +

--- LambdaAndLinq/Ex4/Program.cs
+++ LambdaAndLinq/Ex4/Program.cs
@@ -10,6 +10,28 @@ namespace Ex4
     {
         static void Main(string[] args)
         {
+            Dictionary<string, Dictionary<string, int>> playersDatabase = new Dictionary<string, Dictionary<string, int>>();
+            string line = Console.ReadLine();
+            while (line != "stop the game")
+            {
+                string[] rawData = line.Split('|').ToArray();
+                if (!playersDatabase.ContainsKey(rawData[1]))
+                {
+                    playersDatabase[rawData[1]] =
+                        new Dictionary<string, int> { { rawData[0], int.Parse(rawData[2]) } };
+                }
+                else if(playersDatabase[rawData[1]].Count <= 3){
+                    playersDatabase[rawData[1]].Add(rawData[0], int.Parse(rawData[2]));
+                }
+                line = Console.ReadLine();
+            }
+            foreach( var currTeam in playersDatabase
+                .Where(kvp => kvp.Value.Count >= 3)
+                .OrderByDescending(kvp => kvp.Value.Values.Sum())
+                .ThenByDescending(kvp => kvp.Value.Values))
+            {
+                Console.WriteLine($"{currTeam.Key} - {currTeam.Value.Values.Sum()}");
+            }
         }
     }
 }
