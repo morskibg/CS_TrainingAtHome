ce2251696b63d6a117b167eac15554205db750ba
morskibg <dimityrp@yahoo.com>
2017-10-10 17:43:30 +0300

    +

---
+++ LambdaAndLinq_More/1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            double average = nums.Average();
+            string command = Console.ReadLine();
+            if(command == "All")
+            {
+                Console.WriteLine(string.Join(" ", nums.Where(x => x >= average).OrderBy(x => x)));
+            }
+            else if(command == "Min")
+            {
+                Console.WriteLine(string.Join(" ", nums.Where(x => x >= average).OrderBy(x => x).Take(1)));
+            }
+            else if (command == "Max")
+            {
+                Console.WriteLine(string.Join(" ", nums.Where(x => x >= average).OrderByDescending(x => x).Take(1)));
+            }
+        }
+    }
+}
---
+++ LambdaAndLinq_More/2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int toSkip = commands[0];
+            int toTake = commands[1];
+            int[] processedNums = nums                          
+                .Where(x => x >= 65)
+                .Where(x => x <= 90)                
+                .Skip(toSkip)
+                .Take(toTake)
+                .ToArray();
+            string result = "";
+            foreach(int num in processedNums)
+            {
+                result += (char)num;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
