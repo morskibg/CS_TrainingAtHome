fe58c11388c3eb3fe6087f5402e750e2aa22d5cb
morskibg <dimityrp@yahoo.com>
2017-10-15 17:42:00 +0200

    +

---
+++ temp/temp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temp1
+{
+    class Herro
+    {
+        public string Name { get; set; }
+                     
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = "abnie";
+            string line2 = "anie";
+            Console.WriteLine(line1.CompareTo(line2));
+        }
+    }
+}
