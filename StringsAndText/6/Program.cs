fd8571f4ea3aa9b29ab03062b6c1070e9a97279e
morskibg <dimityrp@yahoo.com>
2017-10-24 18:06:04 +0300

    +

---
+++ StringsAndText/6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static string AddBigNums(string firstNum, string secondNum)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            string firstNum = Console.ReadLine();
+            string secondNum = Console.ReadLine();
+            if(firstNum.Length >= secondNum.Length)
+            {
+                Console.WriteLine(AddBigNums(firstNum, secondNum));
+            }
+            else
+            {
+                Console.WriteLine(AddBigNums(secondNum, firstNum));
+            }
+        }
+    }
+}
