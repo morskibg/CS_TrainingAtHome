be811338e6d3a5a31dc395915fefcb4c6bf7d242
morskibg <dimityrp@yahoo.com>
2017-10-27 13:20:34 +0300

    +

---
+++ RegexLab/2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(^| )\+359(?<del>-| )[2]\k<del>\d{3}\k<del>\d{4}\b";
+            MatchCollection numbers = Regex.Matches(input, pattern);
+            var matchedPhones = numbers.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
