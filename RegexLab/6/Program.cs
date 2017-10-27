d7a975aeeb7237fb443071e6bc0d9b69428b5edd
morskibg <dimityrp@yahoo.com>
2017-10-27 17:10:41 +0300

    +

---
+++ RegexLab/6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<string> inputLines = new List<string>();
+            while(input != "end")
+            {
+                inputLines.Add(input);
+                input = Console.ReadLine();
+            }
+            st
+        }
+    }
+}
