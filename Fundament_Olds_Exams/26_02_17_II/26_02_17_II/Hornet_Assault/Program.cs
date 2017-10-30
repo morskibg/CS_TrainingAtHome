90a311f77b208a06510d76703269859eb6fcfcd6
morskibg <dimityrp@yahoo.com>
2017-10-30 23:06:57 +0200

    +

---
+++ Fundament_Olds_Exams/26_02_17_II/26_02_17_II/Hornet_Assault/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hornet_Assault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> beeHives = Console.ReadLine().Split().Select(long.Parse).ToList();
+            List<long> hornets = Console.ReadLine().Split().Select(long.Parse).ToList();
+            
+            for (int i = 0; i < beeHives.Count; ++i)
+            {
+                long hornetAttack = hornets.Sum();
+                if (hornetAttack > beeHives[i])
+                {
+                    beeHives[i] = -1;
+                }
+                else
+                {
+                    beeHives[i] -= hornetAttack;
+                    hornets.RemoveAt(0);
+                    if (hornets.Count == 0)
+                    {
+                        break;
+                    }
+                    hornetAttack = hornets.Sum();
+                }
+            }
+            if (beeHives.Sum() > 0)
+            {
+                foreach (var hive in beeHives.Where(x => x > 0))
+                {
+                    Console.Write(hive + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", hornets));
+            }
+            Console.WriteLine();
+        }
+    }
+}
