92713cbc65fca8f0f75788df44e4b145617d05c3
morskibg <dimityrp@yahoo.com>
2017-10-30 22:25:54 +0200

    +

---
+++ Fundament_Olds_Exams/09_17_I/PokeMon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int tiredFactor = int.Parse(Console.ReadLine());
+            double halfPokePower = pokePower / 2.0;
+            int pokedCounter = 0;
+            while (pokePower >= distance)
+            {
+                ++pokedCounter;
+                pokePower -= distance;
+                if (pokePower == halfPokePower && tiredFactor > 0)
+                {
+                    pokePower /= tiredFactor;
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokedCounter);
+        }
+    }
+}
---
+++ Fundament_Olds_Exams/09_17_I/pokemonDontGo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemonDontGo
+{
+    class Program
+    {
+        static void RearangeList(ref List<long>poks, long value)
+        {
+            poks = poks
+                .Select(x => x <= value ? x += value : x -= value)
+                .ToList();
+        }
+        static void Main(string[] args)
+        {
+            List<long> pokemons = Console.ReadLine().Split().Select(long.Parse).ToList();
+            long sumOfRemoved = 0;
+            while (pokemons.Count > 0)
+            {
+                long index = long.Parse(Console.ReadLine());
+                long removedElementValue = 0;
+                if (index < 0 || index >= pokemons.Count)
+                {
+                     removedElementValue = index < 0 ? pokemons.First() : pokemons.Last();
+                    if (index < 0)
+                    {
+                        pokemons[0] = pokemons.Last();
+                    }
+                    else
+                    {
+                        pokemons[pokemons.Count - 1] = pokemons[0];
+                    }
+                }
+                else
+                {
+                    removedElementValue = pokemons[(int)index];
+                    pokemons.RemoveAt((int)index);
+                }
+                RearangeList(ref pokemons, removedElementValue);
+                sumOfRemoved += removedElementValue;
+            }
+            Console.WriteLine(sumOfRemoved);
+        }
+    }
+}
