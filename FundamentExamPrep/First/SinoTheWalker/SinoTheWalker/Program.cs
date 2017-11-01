25a02dc9afea5163f0bb077f28f9dd8c375fb966
morskibg <dimityrp@yahoo.com>
2017-11-01 21:06:13 +0200

    +

---
+++ FundamentExamPrep/First/SinoTheWalker/SinoTheWalker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinoTheWalker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            TimeSpan initialTime = new TimeSpan(int.Parse(tokens[0]), int.Parse(tokens[1]), int.Parse(tokens[2]));
+            double steps = double.Parse(Console.ReadLine()) % 86400;
+            double timeForEachStep = double.Parse(Console.ReadLine()) % 86400;
+            double totalTimeInDays = (steps * timeForEachStep) / (60 * 60 * 24);
+            TimeSpan stepTime = TimeSpan.FromDays(totalTimeInDays);
+            TimeSpan resultTime = initialTime + stepTime;
+            Console.WriteLine($"Time Arrival: {resultTime.Hours.ToString("00")}:{resultTime.Minutes.ToString("00")}:{resultTime.Seconds.ToString("00")}");
+
+
+            int t = 0;
+        }
+    }
+}
---
+++ FundamentExamPrep/First/SinoTheWalker/SoftuniKaraoke/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftuniKaraoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> participats = Console.ReadLine()
+                .Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToList();
+            List<string> songs = Console.ReadLine().Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToList();
+            Dictionary<string, List<string>> awards = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "dawn")
+                {
+                    break;
+                }
+                string[] query = input.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                string wolfSinger = query[0].Trim();
+                string song = query[1].Trim();
+                string award = query[2].Trim();
+
+                if (!awards.ContainsKey(wolfSinger))
+                {
+                    if (participats.Contains(wolfSinger) && songs.Contains(song))
+                    {
+                        awards[wolfSinger] = new List<string> {award};
+                    }
+                }
+                else if (participats.Contains(wolfSinger) && songs.Contains(song) && !awards.Values.Any(x => x.Contains(award)))
+                {
+                    awards[wolfSinger].Add(award);
+                }
+            }
+            if (awards.Count == 0)
+            {
+                Console.WriteLine("No awards");
+                return;
+            }
+            foreach (var wolfSinger in awards.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{wolfSinger.Key}: {wolfSinger.Value.Count} awards");
+                foreach (var award in wolfSinger.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"--{award}");
+                }
+            }
+        }
+    }
+}
