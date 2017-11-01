f797c8946bd4cbf564f6ec94556b3339f7bda9b4
morskibg <dimityrp@yahoo.com>
2017-11-01 22:37:26 +0200

    +

---
+++ FundamentExamPrep/First/SinoTheWalker/03. Endurance Rally/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Endurance_Rally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> pilots = Console.ReadLine().Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim()).ToList();
+            List<double> trackLayoutZones = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+            List<long> checkPointInduces = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+            for (int i = 0; i < pilots.Count; i++)
+            {
+                
+                string currPilot = pilots[i];
+                double fuel = (double) currPilot[0];
+                for (int j = 0; j < trackLayoutZones.Count; j++)
+                {
+                    bool isCheckpoint = checkPointInduces.Contains(j);
+                    if (isCheckpoint && j < trackLayoutZones.Count)
+                    {
+                        fuel += trackLayoutZones[j];
+                    }
+                    else if (j < trackLayoutZones.Count)
+                    {
+                        fuel -= trackLayoutZones[j];
+                    }
+                    if (fuel <= 0)
+                    {
+                        Console.WriteLine($"{currPilot} - reached {j}");
+                       
+                        break;
+                    }
+                }
+                if (fuel > 0)
+                {
+                    Console.WriteLine($"{currPilot} - fuel left {fuel:f2}");
+                }
+
+            }
+        }
+    }
+}
---
+++ FundamentExamPrep/First/SinoTheWalker/04. Winning Ticket/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Winning_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> tickets = Console.ReadLine().Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim()).ToList();
+            for (int i = 0; i < tickets.Count; i++)
+            {
+                string currTicket = tickets[i];
+                if (currTicket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+            }
+
+        }
+    }
+}
