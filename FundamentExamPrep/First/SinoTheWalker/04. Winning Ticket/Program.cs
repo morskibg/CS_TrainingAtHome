bc852bed3003d38149956556275ac5ec20a358eb
morskibg <dimityrp@yahoo.com>
2017-11-02 20:03:12 +0200

    +

--- FundamentExamPrep/First/SinoTheWalker/04. Winning Ticket/Program.cs
+++ FundamentExamPrep/First/SinoTheWalker/04. Winning Ticket/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _04.Winning_Ticket
@@ -10,17 +11,47 @@ namespace _04.Winning_Ticket
     {
         static void Main(string[] args)
         {
-            List<string> tickets = Console.ReadLine().Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                .Select(x => x.Trim()).ToList();
-            for (int i = 0; i < tickets.Count; i++)
+            
+            var tickets = Console.ReadLine()
+                .Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var ticket in tickets)
             {
-                string currTicket = tickets[i];
-                if (currTicket.Length != 20)
+                if (ticket.Length != 20)
                 {
                     Console.WriteLine("invalid ticket");
                     continue;
                 }
 
+                var noMatch = true;
+
+                foreach (var winChar in "@#$^".ToCharArray())
+                {
+                    var winReg = new Regex($@"[\{winChar}]{{6,}}");
+
+                    var part1 = winReg.Match(ticket.Substring(0, 10));
+                    var part2 = winReg.Match(ticket.Substring(10, 10));
+
+                    if (!part1.Success || !part2.Success)
+                    {
+                        continue;
+                    }
+
+                    var matchLength = Math.Min(part1.Length, part2.Length);
+
+                    Console.WriteLine(matchLength < 10
+                        ? $"ticket \"{ticket}\" - {matchLength}{winChar}"
+                        : $"ticket \"{ticket}\" - {matchLength}{winChar} Jackpot!");
+
+                    noMatch = false;
+
+                    break;
+                }
+
+                if (noMatch)
+                {
+                    Console.WriteLine($"ticket \"{ticket}\" - no match");
+                }
             }
 
         }
