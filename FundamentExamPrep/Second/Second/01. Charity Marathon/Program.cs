a4b9062d67f3230b72f84e9883b38e2276cfccb9
morskibg <dimityrp@yahoo.com>
2017-11-02 21:00:03 +0200

    +

---
+++ FundamentExamPrep/Second/Second/01. Charity Marathon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Charity_Marathon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int marathonDays = int.Parse(Console.ReadLine());
+            long runnersCount = long.Parse(Console.ReadLine());
+            int laps = int.Parse(Console.ReadLine());
+            long lapLength = long.Parse(Console.ReadLine());
+            int trackCapacity = int.Parse(Console.ReadLine());
+            double moneyPerKm = double.Parse(Console.ReadLine());
+
+            long totalRunners = runnersCount <= trackCapacity * marathonDays ? runnersCount : trackCapacity * marathonDays;
+            
+            long totalDistance = totalRunners * laps * lapLength;
+            double totalRaisedMoney = moneyPerKm * totalDistance / 1000;
+            Console.WriteLine($"Money raised: {totalRaisedMoney:f2}");
+
+        }
+    }
+}
---
+++ FundamentExamPrep/Second/Second/02. Ladybugs/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Ladybugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] field = new int[fieldSize];
+            List<long> initaialIndices = Console.ReadLine().Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToList();
+            foreach (var index in initaialIndices)
+            {
+                field[index] = 1;
+            }
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                string[] query = input.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                long ladybugIndex = long.Parse(query[0]);
+                string command = query[1].Trim();
+                long flyLength = long.Parse(query[2]);
+                while (true)
+                {
+                    if (ladybugIndex >= 0 && ladybugIndex < fieldSize)
+                    {
+                        if (field[ladybugIndex] == 1)
+                        {
+                            long resultIdx = 0;
+                            if (command == "right")
+                            {
+                                resultIdx = ladybugIndex + flyLength;
+                                if (resultIdx < fieldSize)
+                                {
+                                    if (field[resultIdx] == 0)
+                                    {
+                                        field[resultIdx] = 1;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        flyLength += flyLength;
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
