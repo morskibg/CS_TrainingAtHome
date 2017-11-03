8d849469f1843a954d0940ecf2072272cb454a70
morskibg <dimityrp@yahoo.com>
2017-11-03 22:52:16 +0200

    +

---
+++ FundamentExamPrep/Second/Second/04. Roli The Coder/04. Roli The Coder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04.Roli_The_Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.Equals("Time for Code"))
+                {
+                    break;
+                }
+                string ID = Regex.Match(input, @"^(.+?)").Value.Trim();
+                string eventName = Regex.Match(input, @"(?<=\#)(.+?(?=\s))").Value.Trim();
+                var participants = Regex.Matches(input, @"(?<=\@).+?(?=\s|$)").Cast<Match>().Select(x => x.Value.Trim())
+
+                    .ToList();
+
+                int t = 0;
+
+            }
+           
+        }
+    }
+}
