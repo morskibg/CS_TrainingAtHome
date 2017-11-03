8cb32b76a538970a482ada2a66eb6b0be4d8c41d
morskibg <dimityrp@yahoo.com>
2017-11-03 15:08:18 +0200

    +

--- FundamentExamPrep/Second/Second/02. Ladybugs/Program.cs
+++ FundamentExamPrep/Second/Second/02. Ladybugs/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Numerics;
 
 namespace _02.Ladybugs
 {
@@ -12,8 +11,6 @@ namespace _02.Ladybugs
     {
         static void Main(string[] args)
         {
-            
-
             int fieldSize = int.Parse(Console.ReadLine());
             int[] field = new int[fieldSize];
             List<long> initaialIndices = Console.ReadLine().Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
@@ -21,7 +18,11 @@ namespace _02.Ladybugs
                 .ToList();
             foreach (var index in initaialIndices)
             {
-                field[index] = 1;
+                if(index >= 0 && index < fieldSize)
+                {
+                    field[index] = 1;
+                }
+                
             }
             while (true)
             {
@@ -38,20 +39,20 @@ namespace _02.Ladybugs
                 }
                 field[ladybugIndex] = 0;
                 string command = query[1].Trim();
-                BigInteger flyLength = BigInteger.Parse(query[2]);
+                long flyLength = long.Parse(query[2]);
                 while (true)
                 {
 
-                    BigInteger landingIdx = command == "right" ?
+                    long landingIdx = command == "right" ?
                         ladybugIndex + flyLength :
                         ladybugIndex - flyLength;
-                    if(landingIdx < 0 || landingIdx >= fieldSize)
+                    if (landingIdx < 0 || landingIdx >= fieldSize)
                     {
                         break;
                     }
-                    if(field[(int)landingIdx] == 0)
+                    if (field[landingIdx] == 0)
                     {
-                        field[(int)landingIdx] = 1;
+                        field[landingIdx] = 1;
                         break;
                     }
                     flyLength += flyLength;
