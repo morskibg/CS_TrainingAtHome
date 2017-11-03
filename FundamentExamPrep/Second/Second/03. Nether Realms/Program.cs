9a12d5757195faa8185c5aea0b997aefb87af9c6
morskibg <dimityrp@yahoo.com>
2017-11-03 22:11:29 +0200

    +

---
+++ FundamentExamPrep/Second/Second/03. Nether Realms/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03.Nether_Realms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demons = Regex
+                .Matches(Console.ReadLine(), @"([^ ,]+)")
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .OrderBy(x => x)
+                .ToList();
+
+            foreach (var demon in demons)
+            {
+                var health = Regex.Matches(demon, @"([^ +-.*/0-9])")
+                    .Cast<Match>()
+                    .Select(x => (int) x.Value[0])
+                    .Sum();
+
+                var damage = Regex.Matches(demon, @"-?\d+(?:\.\d+)?")
+                    .Cast<Match>()
+                    .Select(x => double.Parse(x.Value))
+                    .Sum();
+
+                var mult = Regex.Matches(demon, @"(\*)").Count;
+
+                var div = Regex.Matches(demon, @"(\/)").Count;
+
+                damage *= Math.Pow(2, mult);
+
+                damage /= Math.Pow(2, div);
+
+                Console.WriteLine($"{demon} - {health} health, {damage:F2} damage");
+
+            }
+        }
+    }
+}
---
+++ FundamentExamPrep/Second/Second/temp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.Nether_Realms
+{
+    class Program
+    {
+        static void Main()
+        {
+            var demons = Regex
+                .Matches(Console.ReadLine(), @"([^ ,]+)")
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .OrderBy(x => x)
+                .ToList();
+
+            foreach (var demon in demons)
+            {
+                var health = Regex.Matches(demon, @"([^ \+\-\.\*\/0-9])")
+                    .Cast<Match>()
+                    .Select(x => (int)x.Value.ToCharArray()[0])
+                    .Sum();
+                   
+
+                var damage = Regex.Matches(demon, @"-?\d+(?:\.\d+)?")
+                    .Cast<Match>()
+                    .Select(x => double.Parse(x.Value))
+                    .Sum();
+
+                var mult = Regex.Matches(demon, @"(\*)").Count;
+
+                var div = Regex.Matches(demon, @"(\/)").Count;
+
+                damage *= Math.Pow(2, mult);
+
+                damage /= Math.Pow(2, div);
+
+                Console.WriteLine($"{demon} - {health} health, {damage:F2} damage");
+            }
+        }
+    }
+}
