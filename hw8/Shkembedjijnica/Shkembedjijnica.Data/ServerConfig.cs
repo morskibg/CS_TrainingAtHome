570af83f434521cab1d60aa8bec693c84c149389
morskibg <dimityrp@yahoo.com>
2018-07-25 16:49:03 +0300

    Remove duplicated directory

--- hw8/Shkembedjijnica/Shkembedjijnica.Data/ServerConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace P01_BillsPaymentSystem.Data
-{
-    public class ServerConfig
-    {
-        public const string ConnectionString = "Server=DESKTOP-QFU7S9K;Database=Shkembedjijnica;Trusted_Connection=True;";
-    }
-}
